dec7465d719c6b110656e209f0faa7d7e85b2eda
antonioeduthz <antonioedu.thz@gmail.com>
2026-03-11 18:24:52 -0300

    Layout changed

---
+++ my-app/src/components/sections/hero.tsx
@@ -0,0 +1,8 @@
+export function Hero() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet</h1>
+      <button className="m-5 bg-white text-black w-35 h-10 outline-1 outline-white outline-offset-8">button</button>
+    </div>
+  );
+}
